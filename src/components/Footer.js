0cb71280f3502f1f465048a70b2fdee1280f0c8f
Ivana Livaja <ivana.livaja@hotmail.com>
2021-02-06 22:38:23 +0100

    footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,23 @@ import logo from '../assets/images/logo.svg';
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<a href="/">
-				<img src={logo} alt="shortly logo" className="footer__logo" />
+			<div className="footer__container container">
+			<a href="/" className="footer__logo">
+				<img src={logo} alt="shortly logo" />
 			</a>
-			<ul>
+			<ul className="footer__links">
 				<li><h4 className="footer__link-title">Features</h4></li>
 				<li><a href="#link-shortening" className="footer__link">Link Shortening</a></li>
 				<li><a href="#branded-links" className="footer__link">Branded Links</a></li>
 				<li><a href="#analytics-links" className="footer__link">Analytics</a></li>
 			</ul>
-			<ul>
+			<ul className="footer__links">
 				<li><h4 className="footer__link-title">Resources</h4></li>
 				<li><a href="#blog" className="footer__link">Blog</a></li>
 				<li><a href="#developers" className="footer__link">Developers</a></li>
 				<li><a href="#support" className="footer__link">Support</a></li>
 			</ul>
-			<ul>
+			<ul className="footer__links">
 				<li><h4 className="footer__link-title">Company</h4></li>
 				<li><a href="#about" className="footer__link">About</a></li>
 				<li><a href="#team" className="footer__link">Our Team</a></li>
@@ -31,6 +32,7 @@ const Footer = () => {
 				<li><span className="footer__icon icon-pinterest"/></li>
 				<li><span className="footer__icon icon-instagram last"/></li>
 			</ul>
+			</div>
 		</footer>
 	);
 };
